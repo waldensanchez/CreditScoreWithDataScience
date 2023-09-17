1747f4bdf8a9fa1e478aa6c5db365af83c1c9405
waldensanchez <walden.sanch@gmail.com>
2023-09-17 16:57:38 -0600

    Process

--- cleanData.py
+++ cleanData.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 def cleanMIS(df: pd.DataFrame):
-    df['Monthly_Inhand_Salary']= df.groupby('Custumer_ID')[['Monthly_Inhand_Salary']].ffill().set_index('ID')
+    df['Monthly_Inhand_Salary']= df.groupby('Customer_ID')['Monthly_Inhand_Salary'].ffill()
     return df
 
 def cleanAI(df: pd.DataFrame):
@@ -10,12 +10,12 @@ def cleanAI(df: pd.DataFrame):
     return df
 
 def dropUselessInfo(df: pd.DataFrame):
-    df.drop(['Name', 'SSN', 'Num_Bank_Accounts', 'Month',])
+    df.drop(['Name', 'SSN', 'Num_Bank_Accounts', 'Month'], axis = 1)
     return df
 
 def cleanAge(df:pd.DataFrame):
     df['Age'] = df['Age'].apply(lambda x: int(x.split('_')[0])).apply(lambda x: np.nan if x < 18 else x)
-    df['Age'] = df['Age'].reset_index().groupby('Customer_ID')[['ID', 'Age']].ffill().set_index('ID')
+    df = df.reset_index().groupby('Customer_ID')[['ID', 'Age']].ffill().set_index('ID')
     return df
 
 def cleanNCC(df: pd.DataFrame):
@@ -26,9 +26,16 @@ def cleanNOF(df: pd.DataFrame):
     df['Num_of_Loan'] = df['Num_of_Loan'].apply(lambda x: int(x.split('_')[0])).apply(lambda x: 0 if x < 0 else x)
     return df
 
+def clean_date(x):
+    try:
+        divided = x.split(" ")
+        return int(divided[0])
+    except:
+        return x
+
 def cleanCHA(df: pd.DataFrame):
-    df['Credit_History_Age'] = df['Credit_History_Age'].apply(lambda x: int(x.split(' ')[0]) if x != 'NA' else x)
-    df['Credit_History_Age'] = df['Credit_History_Age'].reset_index().groupby(['Customer_ID'])[['ID', 'Credit_History_Age']].ffill().set_index('ID')
+    df['Credit_History_Age'] = df['Credit_History_Age'].apply(clean_date)
+    df['Credit_History_Age'] = df.reset_index().groupby(['Customer_ID'])[['ID','Credit_History_Age']].ffill().set_index('ID')
     return df
 
 def clean(df: pd.DataFrame):
@@ -40,3 +47,4 @@ def clean(df: pd.DataFrame):
     cleanNCC(df)
     cleanNOF(df)
     cleanCHA(df)
+    return df

d52d97e937057fc9753be5a7a661e3753f0fed38
waldensanchez <walden.sanch@gmail.com>
2023-10-29 18:47:26 -0600

    data.csv

--- cleanData.py
+++ cleanData.py
@@ -1,66 +1,98 @@
 import pandas as pd
 import numpy as np
 
-def dropUselessInfo(df: pd.DataFrame) -> pd.DataFrame:
-    df.drop(columns = ['Name', 'Age', 'SSN', 'Num_Bank_Accounts', 'Month', 'Occupation', 'Type_of_Loan', 'Payment_of_Min_Amount', 'Payment_Behaviour', 'Total_EMI_per_month', 'Credit_Mix', 'Interest_Rate', 'Changed_Credit_Limit', 'Annual_Income', 'Amount_invested_monthly'], inplace=True)
+# Function to drop columns that are not useful
+def dropUselessInfo(df):
+    df.drop(columns=['Name', 'Age', 'SSN', 'Num_Bank_Accounts', 'Month', 'Occupation', 'Type_of_Loan', 'Payment_of_Min_Amount', 'Payment_Behaviour', 'Total_EMI_per_month', 'Credit_Mix', 'Interest_Rate', 'Changed_Credit_Limit', 'Annual_Income', 'Amount_invested_monthly'], inplace=True)
     return df
 
-def cleanMIS(df: pd.DataFrame) -> pd.DataFrame:
-    df['Monthly_Inhand_Salary']= df.groupby('Customer_ID')[['Monthly_Inhand_Salary']].ffill().set_index(df.index)
+# Function to clean 'Monthly_Inhand_Salary' column
+def cleanMIS(df):
+    df['Monthly_Inhand_Salary'] = df.groupby('Customer_ID')['Monthly_Inhand_Salary'].apply(lambda x: x.ffill().bfill())
     return df
 
-def cleanNCC(df: pd.DataFrame) -> pd.DataFrame:
+# Function to cap the 'Num_Credit_Card' values
+def cleanNCC(df):
     df['Num_Credit_Card'] = df['Num_Credit_Card'].apply(lambda x: 11 if x >= 11 else x)
     return df
 
-def cleanNOF(df: pd.DataFrame):
+# Function to clean 'Num_of_Loan' column
+def cleanNOF(df):
     df['Num_of_Loan'] = df['Num_of_Loan'].apply(lambda x: int(x.split('_')[0])).apply(lambda x: 0 if x < 0 else x).apply(lambda x: 10 if x > 10 else x)
     return df
 
-def cleanCHA(df: pd.DataFrame) -> pd.DataFrame:
+# Function to clean 'Credit_History_Age' column
+def cleanCHA(df):
     df['Credit_History_Age'] = df['Credit_History_Age'].fillna('NA')
-    df['Credit_History_Age'] = df['Credit_History_Age'].apply(lambda x: int(str(x).split(' ')[0]) if str(x) != 'NA' else np.nan)
-    df['Credit_History_Age'] = df.groupby(['Customer_ID'])[['Credit_History_Age']].ffill().set_index(df.index)
+    df['Credit_History_Age'] = df['Credit_History_Age'].apply(lambda x: int(str(x).split(' ')[0]) if ' ' in str(x) and str(x) != 'NA' else np.nan)
+    df['Credit_History_Age'] = df.groupby('Customer_ID')['Credit_History_Age'].apply(lambda x: x.ffill().bfill())
     return df
+
+# Function to clean 'Num_of_Delayed_Payment' column
+def safe_convert_to_float(x):
+    try:
+        return float(x.split('_')[0])
+    except ValueError:
+        return np.nan  # Return NaN if conversion fails
 
 def cleanNoDP(df: pd.DataFrame) -> pd.DataFrame:
-    df['Num_of_Delayed_Payment'] = df['Num_of_Delayed_Payment'].fillna('NA')
-    df['Num_of_Delayed_Payment'] = df['Num_of_Delayed_Payment'].apply(lambda x: int(str(x).split('_')[0]) if str(x) != 'NA' else np.nan).apply(lambda x: 0 if x < 0 else x).apply(lambda x: 27 if x > 27 else x)
-    df['Num_of_Delayed_Payment'] = df.groupby('Customer_ID')[['Num_of_Delayed_Payment']].ffill().set_index(df.index)
+    # Replace 'NA' and other non-numeric strings with NaN to use fill methods
+    df['Num_of_Delayed_Payment'] = df['Num_of_Delayed_Payment'].apply(lambda x: safe_convert_to_float(str(x)))
+    
+    # Apply ffill and bfill within groups to fill NaN values
+    df['Num_of_Delayed_Payment'] = df.groupby('Customer_ID')['Num_of_Delayed_Payment'].apply(lambda x: x.ffill().bfill())
+    
+    # Set remaining NaNs to 0 or another sensible default value
+    df['Num_of_Delayed_Payment'].fillna(0, inplace=True)
+    
+    # Ensure values are within the specified range (e.g., 0 to 27)
+    df['Num_of_Delayed_Payment'] = df['Num_of_Delayed_Payment'].clip(lower=0, upper=27)
+    
     return df
 
-def cleanOD(df: pd.DataFrame) -> pd.DataFrame:
+
+# Function to clean 'Outstanding_Debt' column
+def cleanOD(df):
     df['Outstanding_Debt'] = df['Outstanding_Debt'].apply(lambda x: float(str(x).split('_')[0]))
     return df
 
-def cleanMB(df: pd.DataFrame) -> pd.DataFrame:
+# Function to clean 'Monthly_Balance' column
+def cleanMB(df):
     df['Monthly_Balance'] = df['Monthly_Balance'].fillna('NA')
-    df['Monthly_Balance'] = df['Monthly_Balance'].apply(lambda x: float(str(x).split('__')[1]) if str(x)[0] == '_' else str(x))
-    df['Monthly_Balance'] = df['Monthly_Balance'].apply(lambda x: float(x) if x != 'NA' else np.nan)
-    df['Monthly_Balance'] = df['Monthly_Balance'].apply(lambda x: 0 if x < 0 else x)
-    df['Monthly_Balance'] = df.groupby(['Customer_ID'])[['Monthly_Balance']].ffill().set_index(df.index)
+    df['Monthly_Balance'] = df['Monthly_Balance'].apply(lambda x: float(str(x).split('__')[1]) if '__' in str(x) and str(x)[0] == '_' else (float(x) if x != 'NA' else np.nan))
+    df['Monthly_Balance'] = df.groupby('Customer_ID')['Monthly_Balance'].apply(lambda x: x.ffill().bfill())
     return df
 
-def cleanNCI(df: pd.DataFrame) -> pd.DataFrame:
-    df['Num_Credit_Inquiries'] = df.groupby(['Customer_ID'])[['Num_Credit_Inquiries']].ffill().set_index(df.index)
+# Function to cap 'Num_Credit_Inquiries' values
+def cleanNCI(df):
+    df['Num_Credit_Inquiries'] = df.groupby('Customer_ID')['Num_Credit_Inquiries'].apply(lambda x: x.ffill().bfill())
     df['Num_Credit_Inquiries'] = df['Num_Credit_Inquiries'].apply(lambda x: 18 if x > 18 else x)
     return df
 
-def scoreToNum(df: pd.DataFrame) -> pd.DataFrame:
+# Function to map scores to numbers
+def scoreToNum(df):
     df['Score'] = df['Credit_Score'].map({'Poor': 1, 'Standard': 2, 'Good': 3})
     return df
 
-def clean(df: pd.DataFrame) -> pd.DataFrame:
+# Main cleaning function that applies all the cleaning steps
+def clean(df):
+    df = df.copy()  # Create a copy to avoid modifying the original dataframe
     df = df.set_index('ID')
-    dropUselessInfo(df)
-    cleanMIS(df)
-    cleanNCC(df)
-    cleanNOF(df)
-    cleanCHA(df)
-    cleanNoDP(df)
-    cleanOD(df)
-    cleanMB(df)
-    scoreToNum(df)
-    cleanNCI(df)
-    df = df.drop('Customer_ID', axis=1)
+    df = dropUselessInfo(df)
+    df = cleanMIS(df)
+    df = cleanNCC(df)
+    df = cleanNOF(df)
+    df = cleanCHA(df)
+    df = cleanNoDP(df)
+    df = cleanOD(df)
+    df = cleanMB(df)
+    df = scoreToNum(df)
+    df = cleanNCI(df)
+    df = df.drop('Customer_ID', axis=1).reset_index(drop=True)
     return df
+
+# Sample usage:
+# Load your data
+# data = pd.read_csv('train.csv')
+# Clean your data
+# cleaned_data = clean(data)
